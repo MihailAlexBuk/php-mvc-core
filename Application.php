8492a4c1d4617b8b881221f40b09d223eaf0cfd3
Mihail <mihail.alexbuk@gmail.com>
2023-02-13 05:15:38 -0500

    Initial commit

---
+++ Application.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace app\core;
+
+use app\core\db\Database;
+use app\core\db\DbModel;
+
+class Application
+{
+    public static string $ROOT_DIR;
+
+    public $userClass;
+    public string $layout = 'main';
+    public Router $router;
+    public View $view;
+    public Database $db;
+    public Request $request;
+    public Response $response;
+    public Session $session;
+    public ?UserModel $user;
+
+    public static Application $app;
+    public ?Controller $controller = null;
+
+    public function __construct($rootPath, array $config)
+    {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
+
+        $this->userClass = $config['userClass'];
+
+        $this->request = new Request();
+        $this->response = new Response();
+        $this->session = new Session();
+        $this->view = new View();
+        $this->router = new Router($this->request, $this->response);
+
+        $this->db = new Database($config['db']);
+
+        $primaryValue = $this->session->get('user');
+        if($primaryValue){
+            $primaryKey = $this->userClass::primaryKey();
+            $this->user = $this->userClass::findOne([$primaryKey => $primaryValue]);
+        }else{
+            $this->user = null;
+        }
+    }
+
+    public function run()
+    {
+        try{
+            echo $this->router->resolve();
+        }catch (\Exception $e){
+            $this->response->setStatusCode($e->getCode());
+            echo $this->view->renderView("_error", [
+                'exception' => $e
+            ]);
+        }
+    }
+
+    public function getController(): Controller
+    {
+        return $this->controller;
+    }
+
+    public function setController(Controller $controller): void
+    {
+        $this->controller = $controller;
+    }
+
+    public function isGuest()
+    {
+        return !self::$app->user;
+    }
+
+    public function login(UserModel $user)
+    {
+        $this->user = $user;
+        $primaryKey = $user->primaryKey();
+        $primaryValue = $user->{$primaryKey};
+        $this->session->set('user', $primaryValue);
+        return true;
+    }
+
+    public function logout()
+    {
+        $this->user = null;
+        $this->session->remove('user');
+    }
+}
---
+++ View.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace app\core;
+
+
+class View
+{
+
+    public string $title = "";
+
+    public function renderView($view, $params = [])
+    {
+        $viewContent = $this->renderOnlyView($view, $params);
+        $layoutsContent = $this->layoutsContent();
+        return str_replace('{{content}}', $viewContent, $layoutsContent);    }
+
+    public function renderContent($viewContent)
+    {
+        $layoutsContent = $this->layoutsContent();
+        return str_replace('{{content}}', $viewContent, $layoutsContent);
+    }
+
+    protected function layoutsContent()
+    {
+        $layout = Application::$app->layout;
+        if(Application::$app->controller){
+            $layout = Application::$app->controller->layout;
+        }
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/$layout.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view, $params)
+    {
+        foreach ($params as $key => $value)
+        {
+            $$key = $value;
+        }
+
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
+
+}
